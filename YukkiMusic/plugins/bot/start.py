0dd1da7bd0e8fb7d51c16588528096cfcff25ce0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-17 11:00:47 +0530

    mini revert in start

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -178,15 +178,26 @@ async def start_comm(client, message: Message, _):
         except:
             OWNER = None
         out = private_panel(_, app.username, OWNER)
-        await message.reply_photo(
+        if config.START_IMG_URL:
+            try:
+                await message.reply_photo(
                     photo=config.START_IMG_URL,
-                    caption=_["start_2"].format(message.from_user.mention, app.mention),
+                    caption=_["start_2"].format(app.mention),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
-            
+            except:
+                await message.reply_text(
+                    _["start_2"].format(app.mention),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+        else:
+            await message.reply_text(
+                _["start_2"].format(app.mention),
+                reply_markup=InlineKeyboardMarkup(out),
+            )
         if await is_on_off(config.LOG):
             sender_id = message.from_user.id
-            sender_name = message.from_user.username
+            sender_name = message.from_user.first_name
             return await app.send_message(
                 config.LOG_GROUP_ID,
                 f"{message.from_user.mention} has just started Bot.\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
